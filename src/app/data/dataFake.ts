8edd5926f8a5269b663fdcf796600e4d415e797a
Inaldo Macena <inaldomacena19@gmail.com>
2024-02-25 04:27:59 -0300

    correções

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -4,27 +4,27 @@ export const dataFake = [
     photo:
       'https://hermes.dio.me/tracks/62ed1f1d-8d76-4bbc-905f-e73d20cb82f5.png',
     title: 'Curso Formação HTML 5',
-    description: '',
+    description: 'Essa é uma excelente formação para começar sua carreira como front-end.',
   },
   {
     id: '2',
     photo:
       'https://hermes.dio.me/tracks/da043c7a-7189-441e-bf28-adc2d05a4934.png',
     title: 'Curso Formação CSS 3',
-    description: '',
+    description: 'Dê o próximo passo na sua jornada no mundo do Front-end aprendendo a estilizar e turbinar ainda mais suas páginas Web com Cascading Style Sheets, o famoso CSS.',
   },
   {
     id: '3',
     photo:
       'https://i.ytimg.com/vi/sBirK_DGM8A/maxresdefault.jpg',
     title: ' Curso JavaScript Developer',
-    description: '',
+    description: 'Aprenda JavaScript do zero, em uma abordagem de ensino totalmente imersiva e prática. Nesta formação você aprenderá desde os fundamentos desta linguagem de programação até o seu papel essencial na Web atualmente.',
   },
   {
     id: '4',
     photo:
       'https://hermes.dio.me/files/assets/0ed55d2d-deb4-4bbc-a92c-c1d223849cc1.png',
-    title: 'Curso Lógica de Progamação',
-    description: 'Estudando um pouco todo dia eu irei longe!',
+    title: 'Curso Lógica de Programação',
+    description: 'Inicie com os princípios da lógica de programação, começando por conceitos introdutórios, desde a exploração de algoritmos, variáveis e estruturas condicionais até a implementação de estruturas de repetição e operadores lógicos e de comparação.',
   },
 ];
